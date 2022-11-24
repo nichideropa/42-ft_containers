c614a81e1f06dcb3d48c190e50de43a8369c87fd
Rilind Jasari <rjasari@1-E-9.42heilbronn.de>
2022-11-24 15:12:09 +0100

    lots of questions regarding compatibility with c++98

---
+++ main.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <string>
+#include <cstddef> // std::size_t
+
+/* all standard containers*/
+/* sequence */
+// #include <array>		// c++11
+#include <vector>
+#include <deque>
+// #include <forward_list>	// c++11
+#include <list>
+/* adapters*/
+#include <stack>
+#include <queue>
+/* associative */
+#include <map>
+#include <set>
+
+
+/* own container */
+#include "stack.hpp"
+
+
+# define GREEN	"\033[32m"
+# define RESET	"\033[0m"
+# define PRINT(_msg)(std::cout << GREEN << _msg << RESET << std::endl)
+
+// overloaded output operators
+std::ostream &operator<<(std::ostream &os, std::stack<int> s)
+{
+	std::stack<int>::size_type	_size = s.size();
+	for (std::stack<int>::size_type i = 0; i < _size; ++i)
+	{
+		os << s.top() << ' ';
+		s.pop();
+	}
+	return (os);
+}
+
+std::ostream &operator<<(std::ostream &os, ft::stack<int> f)
+{
+	ft::stack<int>::size_type	_size = f.size();
+	for (ft::stack<int>::size_type i = 0; i < _size; ++i)
+	{
+		os << f.top() << ' ';
+		f.pop();
+	}
+	return (os);
+}
+
+
+
+/* mains */
+
+int	main()
+{
+	PRINT("STD:");
+	std::stack<int>		s1;
+
+	s1.push(3);
+	s1.push(2);
+	s1.push(1);
+	std::cout << s1 << std::endl;
+
+	// copy constructor
+	std::stack<int>		s2(s1);
+	std::cout << s2 << std::endl;
+
+	// assignment operator
+	std::stack<int>		s3;
+	s3 = s2;
+
+	std::cout << s3 << std::endl;
+
+	std::stack<int>		s4(std::stack<int>::reference cntr);
+
+	PRINT("");
+	PRINT("FT:");
+	ft::stack<int>		f1;
+
+	f1.push(3);
+	f1.push(2);
+	f1.push(1);
+	std::cout << f1 << std::endl;
+
+	ft::stack<int>		f2(f1);
+	std::cout << f2 << std::endl;
+
+	ft::stack<int>		f3;
+	f3 = f2;
+	std::cout << f3 << std::endl;
+
+	ft::stack<int>		f4(std::deque<int>::reference cntr);
+}
--- stack.hpp
+++ stack.hpp
@@ -1,6 +1,76 @@
 #ifndef STACK_HPP
 # define STACK_HPP
 
+// instead of define possible
+// #pragma once
 
+// using std::vector as default container for now
+// must be replaced by ft::vector
+#include <vector>
+// #include "vector.hpp"
+
+namespace ft {
+
+	template <typename T, typename Container=std::deque<T> >
+	class stack
+	{
+		protected:
+			Container		c;
+		public:
+			/* typename specifier to differentiate between value, type and template */
+			typedef Container									container_type;
+			typedef typename container_type::value_type			value_type;
+			typedef typename container_type::size_type			size_type;
+			// typedef typename container_type::reference			reference;				// c++11?
+			// typedef typename container_type::const_reference	const_reference;		// c++11?
+
+
+			stack() : c()
+			{}
+
+			stack(const stack &other) : c(other.c)
+			{}
+
+			~stack()
+			{}
+
+			stack	&operator=(const stack &other)
+			{
+				c = other.c;
+				return (*this);
+			}
+
+			/* don't mention the internal implementation of the functions
+			you will use your own implemented container as the default underlying of stack */
+			/* sort it to make its appearance look good, ugly now */
+			bool empty()		const	{ return c.empty(); }
+
+			size_type size()	const	{ return c.size(); }
+
+			// reference top()					{ return c.back(); }
+			value_type &top()				{ return c.back(); }
+
+			// const_reference top const		{ return c.back(); }
+			const value_type &top() const	{ return c.back(); }
+
+			void push(const value_type &value)	{ c.push_back(value); }
+
+			void pop()	{ c.pop_back(); }
+	};
+
+}
+
+
+/*** features of c++11 that might be useful ***/
+
+/* the initial implementation of reference and const reference typedef
+typedef typename value_type&			reference;
+typedef typename const value_type&		const_reference;
+*/
+
+/* specific to stack
+typedef typename container_type::reference			reference;
+typedef typename container_type::const_reference	const_reference;
+*/
 
 #endif // STACK_HPP
