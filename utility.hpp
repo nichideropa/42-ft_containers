d9dac76795f0441a0101929b35abb8c8f965f9e1
Rilind Jasari <rjasari@1-E-9.42heilbronn.de>
2022-11-25 15:49:03 +0100

    starting on pair

--- utility.hpp
+++ utility.hpp
@@ -3,27 +3,49 @@
 
 #include <utility>
 
-// everything in a struct is public
-template <typename T1, typename T2>
-struct pair
-{
-	// why is there no need for the typename specifier?
-	/* types */
-	typedef T1			first_type;
-	typedef T2			second_type;
+namespace ft {
 
-	/* attributes */
-	T1			first;
-	T2			second;
+	// everything in a struct is public
+	template <typename T1, typename T2>
+	struct pair
+	{
+		/* types */
+		typedef T1			first_type;
+		typedef T2			second_type;
 
-	/* methods */
-	pair();
-	template <typename U, typename V>
-	pair(const pair<U,V> &p);
-	pair(const first_type &x, const second_type &y);
+		/* attributes */
+		T1			first;
+		T2			second;
 
-	// only in cppreference mentioned
-	pair(const pair &p);
-}
+		/* methods */
+		// cplusplus
+		pair()
+			: first(), second()
+		{}
+
+		pair(const first_type &_a, const second_type &_b)
+			: first(a), second(b)
+		{}
+
+		template <typename U, typename V>
+		pair(const pair<U,V> &_p) : first(p.first), second(p.second)
+		{}
+
+		pair	&operator=(const pair &_p)
+
+		/* copy constructor
+			is defaulted (until C++11 it's implicitly declared */
+		// pair(const pair &p) : first(p.first), second(p.second)
+		// {}
+	};
+
+} // namespace ft
+
+/* doesn't throw exceptions unless one of specified options throws
+	e.G. constructor of an element (no-throw guarantee)
+
+	construction with move semantics may leave explicit copy constructor
+	in invalid state (no guarantees)
+*/
 
 #endif
